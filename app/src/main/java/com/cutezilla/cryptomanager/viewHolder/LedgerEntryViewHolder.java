6d37d150305a43f93112f5e735c969c2d840d549
Faran <farantariq24@gmail.com>
2021-06-29 06:23:28 -0700

    ui fixed and improved

--- app/src/main/java/com/cutezilla/cryptomanager/viewHolder/LedgerEntryViewHolder.java
+++ app/src/main/java/com/cutezilla/cryptomanager/viewHolder/LedgerEntryViewHolder.java
@@ -11,11 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.cutezilla.cryptomanager.Interface.ItemClickListener;
 import com.cutezilla.cryptomanager.R;
 import com.cutezilla.cryptomanager.model.LedgerEntry;
+import com.cutezilla.cryptomanager.util.Common;
 
 public class LedgerEntryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     ItemClickListener itemClickListener;
     View view;
-    TextView status,time,maxBuy,minBuy,totalCur,totalInvested;
+    TextView status,time,maxBuy,minBuy,totalCur,totalInvested,tv_buy_price;
     public void setItemClickListener(ItemClickListener itemClickListener) {
         this.itemClickListener = itemClickListener;
     }
@@ -29,7 +30,7 @@ public class LedgerEntryViewHolder extends RecyclerView.ViewHolder implements Vi
         minBuy = (TextView) itemView.findViewById(R.id.tv_minBuyValue);
         totalCur = (TextView) itemView.findViewById(R.id.tv_currencyAmount);
         totalInvested = (TextView) itemView.findViewById(R.id.tv_total_investedAmount);
-
+        tv_buy_price = (TextView) itemView.findViewById(R.id.tv_buy_price);
     }
 
     @Override
@@ -37,12 +38,16 @@ public class LedgerEntryViewHolder extends RecyclerView.ViewHolder implements Vi
         itemClickListener.onClick(v, getAdapterPosition());
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint({"SetTextI18n", "ResourceAsColor"})
     public void setViewData(LedgerEntry ledgerEntry) {
         status.setText(ledgerEntry.getStatus());
         time.setText(ledgerEntry.getDate());
         totalCur.setText(ledgerEntry.getCryptoAmount() +" " + ledgerEntry.getCurrency());
         totalInvested.setText(String.valueOf(ledgerEntry.getInvestedAmount())+"$");
+        tv_buy_price.setText(String.valueOf(ledgerEntry.getPrice()));
+        if (ledgerEntry.getStatus().equals(Common.STR_SELL)){
+            view.setBackgroundColor(Color.parseColor("#E53935"));
+        }
 
     }
 }
